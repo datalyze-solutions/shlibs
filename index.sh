ac2e86579c5f0d831d9d20e65d9f02cf036323fe
Matthias Ludwig <m.ludwig@datalyze-solutions.com>
2020-02-27 18:00:46 +0100

    Added `index.sh` to centralize import

---
+++ index.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# script="$(readlink --canonicalize-existing "$0")"
+# script_dir=$(dirname ${script})
+
+script_dir=$( dirname "${BASH_SOURCE[0]}")
+for file in $(find ${script_dir} -maxdepth 1 ! -path "${script_dir}/index.sh" -name "*.sh" | sort); do
+    source $file
+    echo >&2 "sourced $file"
+done
