8889e5f159d29e011dd1383e3cd7c7da2e186394
Matthias Ludwig <m.ludwig@datalyze-solutions.com>
2020-03-05 16:13:09 +0100

    `clone_associative_array` function added
    keyfile = bosch-dev-keyfile-433fed4e

--- arrays.sh
+++ arrays.sh
@@ -1,15 +1,31 @@
 #!/usr/bin/env bash
 
-copy_associative_array() {
-    local source=$1
-    local target=$2
+# copy_associative_array() {
+#     local source=$1
+#     local target=$2
 
-    eval $(typeset -A -p $source | sed 's/$source=/$target=/')
-}
+#     eval $(typeset -A -p $source | sed 's/$source=/$target=/')
+# }
 
-copy_array() {
-    local source=$1
-    local target=$2
+# copy_array() {
+#     local source=$1
+#     local target=$2
 
-    target=("${source[@]}")
+#     target=("${source[@]}")
+# }
+
+clone_associative_array() {
+    local source_name="${1}"
+    local target_name="${2}"
+
+    declare -n source_ref="$source_name"
+    declare -n target_ref="$target_name"
+
+    for k in "${!source_ref[@]}"; do
+        # log_debug "$k: ${source_ref[$k]}"
+        target_ref[$k]=${source_ref[$k]}
+    done
+
+    unset -n source_ref
+    unset -n target_ref
 }
