4705a5c38f5273406241a9d6b92d4b04170d5c36
Matthias Ludwig <m.ludwig@datalyze-solutions.com>
2020-03-06 16:27:43 +0100

    Added logging_level, like with known logger frameworks; other enhancements and bug fixed
    * added global LOGGING_LEVEL to specify what level of messages should be
    printed, e.g. LOGGING_LEVEL=debug -> will show all messages;
    LOGGING_LEVEL=warn -> will only show `log_err` and `log_warn` messages
    and skips `log_debug` or `log_info` messages
    * added `log_error` and `log_warning` functions (same as `log_err` and
    `log_warn`)
    * `log` now supports and uses long-opts
    * refactoring and bug fixing
    * new option `--mode|-M` added to specify the shell application to use:
    `echo` or `logger`
    * new option `--no-stats|-X` to skip printing of statistics, like time,
    function, line-number, etc.

--- logging.sh
+++ logging.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
-_logger_tag_base() {
+declare -A LOGGING_LEVELS=([DEBUG]=0 [debug]=0 [INFO]=1 [info]=1 [WARN]=2 [warn]=2 [WARNING]=2 [warning]=2 [ERR]=3 [err]=3 [ERROR]=3 [error]=3)
+
+_stats_base() {
   echo "time=\"$datetime\" level=\"$level\" app=\"$app\""
 }
 
-_logger_tag_info() {
-  echo $(_logger_tag_base)
+_stats_info() {
+  echo $(_stats_base)
   # echo "msg:"
 }
 
-_logger_tag_err() {
-  echo $(_logger_tag_base)
+_stats_err() {
+  echo $(_stats_base)
   echo "file=\"${BASH_SOURCE[$caller_index]}\" line=\"${BASH_LINENO[$caller_index - 1]}\""
 }
 
-_logger_tag_debug() {
-  echo $(_logger_tag_base)
+_stats_debug() {
+  echo $(_stats_base)
   echo "file=\"${BASH_SOURCE[$caller_index]}\" line=\"${BASH_LINENO[$caller_index - 1]}\" function=\"${FUNCNAME[$caller_index]}\""
 }
 
-logger_tag() {
+get_stats() {
   local datetime="$(date '+%Y-%m-%d %H:%M:%S.%4N')" # on alpine install coreutils package to get nanoseconds %N
   local app="${0##*/}"
   local level="${1:-user.info}"
@@ -36,83 +38,186 @@ logger_tag() {
 
   case "$level" in
   user.info)
-    echo $(_logger_tag_info)
+    echo $(_stats_info)
     ;;
   user.debug)
-    echo $(_logger_tag_debug)
+    echo $(_stats_debug)
     ;;
   user.err)
-    echo $(_logger_tag_err)
+    echo $(_stats_err)
     ;;
   user.warn)
-    echo $(_logger_tag_info)
+    echo $(_stats_info)
     ;;
   user.notice)
     echo "time=\"$datetime\""
     ;;
   *)
-    echo $(_logger_tag_info)
+    echo $(_stats_info)
     ;;
   esac
 
 }
 
+count_params() {
+  local optarg_value="$1"
+  # in case of paramless value, OPTARG will be the next parameter with leading '-' char
+  if [ "${optarg_value:0:1}" = "-" ]; then
+    echo 1
+  else
+    echo 2
+  fi
+}
+
+_log_usage() {
+  local function_name="${FUNCNAME[-1]:-log}"
+  cat <<EOF
+Usage:
+    ${function_name:-log} [OPTIONS] [message(s)]
+
+    Enhanced 'echo' to log messages with extra infos
+
+Options
+    -h, --help              show's usage information
+    -M, --mode              the programm to use: 'echo' or 'logger'
+    -X, --no-stats          excludes statistics, e.g. time, line, function, etc.
+    -p, --priority          logger priority
+    -L, --level             level in the LOGGING_LEVELS array
+                            Setting the global variable LOGGING_LEVEL, you can specify if a message should be displayed.
+                            e.g. LOGGING_LEVEL=err -> only the highest logging function will display anything
+                            Levels:
+                              * level 0: DEBUG, debug
+                              * level 1: INFO, info
+                              * level 2: WARN, WARNING, warn, warning
+                              * level 3: ERR, ERROR, err, error
+EOF
+}
+
 log() {
+
+  # translate long options to short
+  args=
+  for arg; do
+    delim=""
+    case "$arg" in
+    --help) args="${args}-h " ;;
+    --mode) args="${args}-M " ;;
+    --priority) args="${args}-p " ;;
+    --level) args="${args}-L " ;;
+    --no-stats) args="${args}-X " ;;
+    # pass through anything else
+    *)
+      [[ "${arg:0:1}" == "-" ]] || delim="\""
+      args="${args}${delim}${arg}${delim} "
+      ;;
+    esac
+  done
+  # reset the translated args
+  eval set -- $args
+
   # https://eklitzke.org/using-local-optind-with-bash-getopts
   local OPTIND ignore
   local invalid_opts=0
-  local quiet=0
+  local valid_opts=0
+  local local_logging_level=0
+  local global_logging_level="${LOGGING_LEVEL:-DEBUG}"
 
-  while getopts ":p:q" opt; do
+  local mode="logger"
+  local show_stats=1
+
+  while getopts ":hM:Xp:L:" opt; do
     case $opt in
-    q)
-      quiet=$((quiet + 1))
-      # quiet=1
+    L)
+      params=$(count_params "${OPTARG}")
+      valid_opts=$((valid_opts + params))
+      local_logging_level="${OPTARG}"
+      ;;
+    M)
+      params=$(count_params "${OPTARG}")
+      valid_opts=$((valid_opts + params))
+      if [ "${OPTARG}" = "logger" ] || [ "${OPTARG}" = "echo" ]; then
+        mode="${OPTARG}"
+      fi
       ;;
     p)
+      params=$(count_params "${OPTARG}")
+      valid_opts=$((valid_opts + params))
       # needs to be logger priority
       priority="${OPTARG:-user.info}"
       ;;
-    \?)
+    X)
+      show_stats=0
+      valid_opts=$((valid_opts + 1))
+      ;;
+    h)
+      _log_usage
+      return
+      ;;
+    *)
+      # echo "invalid option * $OPTARG"
       # count invalid options, e.g. log_info "-d test test"
       # use them as message
       invalid_opts=$((invalid_opts + 1))
       ;;
     esac
   done
-  # remove opts from msg; take invalid opts into calculation
-  shift "$((OPTIND - 1 - invalid_opts))"
+  # shift "$((OPTIND - 1))"
+  shift "$valid_opts"
 
-  local tag="$(logger_tag $priority)"   # name of programm
-  local msg="$@"                        # use all the rest as message
+  local stats="$(get_stats $priority)" # name of programm
+  local msg="$@"                       # use all the rest as message
 
-  if [ "${quiet:-0}" -eq 0 ]; then
-    logger -s -p $priority "$tag msg=\"$msg\""
-  else
-    if [ "${quiet:-0}" -eq 1 ]; then
-      echo >&2 "[$priority] $msg"
+  # check, if we should print messages, depending on global logging level order
+  if ((${LOGGING_LEVELS[$global_logging_level]:-0} > local_logging_level)); then
+    return
+  fi
+
+  case $mode in
+  logger)
+    if [ "${show_stats}" -eq 1 ]; then
+      logger -s -p $priority "$stats msg=\"$msg\""
+    else
+      logger -s -p $priority "msg=\"$msg\""
+    fi
+    ;;
+  echo)
+    if [ "${show_stats}" -eq 1 ]; then
+      echo >&2 "[$priority - $stats] $msg"
     else
       echo >&2 "$msg"
     fi
-  fi
+    ;;
+  *)
+    logger -s -p $priority "$stats msg=\"$msg\""
+    ;;
+  esac
 }
 
+# level 0
 log_debug() {
-  log -p user.debug "$@"
+  log --level 0 --priority user.debug "$@"
 }
 
-log_err() {
-  log -p user.err "$@"
-}
-
+# level 1
 log_info() {
-  log -p user.info "$@"
+  log --level 1 --priority user.info "$@"
 }
 
 log_warn() {
-  log -p user.err "$@"
+  log --level 2 --priority user.warn "$@"
+}
+log_warning() {
+  log --level 2 --priority user.warn "$@"
 }
 
+log_err() {
+  log --level 3 --priority user.err "$@"
+}
+log_error() {
+  log --level 3 --priority user.err "$@"
+}
+
+# show always
 log_notice() {
-  log -p user.notice "$@"
+  log --level 4 --priority user.notice "$@"
 }
