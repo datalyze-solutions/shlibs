914f041239eb03cc2d9506e47326f6e5485d375b
Matthias Ludwig <m.ludwig@datalyze-solutions.com>
2020-07-08 10:00:52 +0200

    Added `--reload|-r` flag to make reload explicit, docker containers might not start otherwise

--- index.sh
+++ index.sh
@@ -3,10 +3,14 @@
 # script="$(readlink --canonicalize-existing "$0")"
 # script_dir=$(dirname ${script})
 
-script_dir=$( dirname "${BASH_SOURCE[0]}")
+script_dir=$(dirname "${BASH_SOURCE[0]}")
 # export PATH=$PATH:$script_dir/bin
 
-source ${script_dir}/unset.sh
+if [[ "$1" == "--reload" || "$1" == "-r" ]]; then
+    echo "Reloading"
+    source ${script_dir}/unset.sh
+fi
+
 for file in $(find -L ${script_dir} -maxdepth 1 ! -path "${script_dir}/index.sh" ! -path "${script_dir}/unset.sh" -name "*.sh" | sort); do
     source $file
     # echo >&2 "sourced $file"
