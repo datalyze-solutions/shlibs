bb710e17a2d17c73179976412a91168fe4b5f713
Matthias Ludwig <m.ludwig@datalyze-solutions.com>
2020-08-14 09:33:32 +0200

    Added user parameter to `wait_for_postgres`

--- wait.sh
+++ wait.sh
@@ -10,9 +10,10 @@ wait_indefinitely() {
 wait_for_postgres() {
     local host="${1:-127.0.0.1}"
     local sleeptime="${2:-5}"
+    local user="${3:-root}"
 
     log_info "Checking for postgres..."
-    until pg_isready -q -h $host; do
+    until pg_isready -U $user -q -h $host; do
         log_err "Postgres is unavailable - retrying $host in a $sleeptime seconds"
         sleep $sleeptime
     done
